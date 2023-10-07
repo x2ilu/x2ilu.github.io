6f6399da746f4e80b91af62c5573d01dd325931c
dost <dinara.osadciya@icloud.com>
2023-10-07 14:34:27 +0400

    add changes html + css + js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function myFunction2(){
+function myFunction1(){
     let a = prompt("enter parametr A?");
 
     let h = prompt("enter parametr H?");
@@ -6,6 +6,17 @@ function myFunction2(){
     let answer = (a*h/2);
 
     let netice = "при пара основании A которое равно " + a + " и при высоте h лежащем на основании A и равному " + h + " площадь будет равна " + answer ;
+
+    document.getElementById("buton1").innerHTML = netice;
+}
+function myFunction2(){
+    let a = prompt("enter parametr A?");
+
+    let h = prompt("enter parametr A?");
+
+    let answer = (a**2);
+
+    let netice = "площадь треугольника равна стороне" + a + " в квадрате" ;
 
     document.getElementById("buton2").innerHTML = netice;
 }
